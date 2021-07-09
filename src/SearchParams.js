50d77472f72de29ab210be5c34e497dbc834f5b3
Daria Lasecka <daria.lasecka@htdevelopers.com>
2021-07-09 18:00:34 +0200

    Added router

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
