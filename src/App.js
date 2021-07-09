33717908eeb819849356fdb78ff1ec63e5e73a2f
Daria Lasecka <daria.lasecka@htdevelopers.com>
2021-07-09 16:28:52 +0200

    Refactor pet list

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-        <SearchParams></SearchParams>
-        {/*
+      <SearchParams></SearchParams>
+      {/*
       <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />*/}
