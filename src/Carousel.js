195051f66b961dc2bbc6edeac0979de681082add
Daria Lasecka <daria.lasecka@htdevelopers.com>
2021-07-16 16:50:54 +0200

    Added first tests

--- src/Carousel.js
+++ src/Carousel.js
@@ -21,7 +21,7 @@ class Carousel extends Component {
 
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal" />
+        <img src={images[active]} data-testid="hero" alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             <img
@@ -29,6 +29,7 @@ class Carousel extends Component {
               src={photo}
               data-index={index}
               onClick={this.handleIndexClick}
+              data-testid={`thumbnail${index}`}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
